f6c3118b143e52abf2dbbef5c01b2d00e7fa3088
roi.peretz <roi081295@gmail.com>
2022-03-01 10:08:12 +0200

    WebView2 + Cefsharp

---
+++ WebControlOverlay.CefSharp/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using CefSharp.Wpf;
+
+namespace www
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OverlayButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var browserElement = FindName("Browser");
+            var browser = browserElement as ChromiumWebBrowser;
+
+            browser.Address = "https://www.ynet.co.il/";
+        }
+    }
+}
---
+++ WebControlOverlay.WebView2/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WebControlOverlay.WebView2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OverlayButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            webView.NavigateToString("https://www.ynet.co.il/home/");
+        }
+    }
+}
